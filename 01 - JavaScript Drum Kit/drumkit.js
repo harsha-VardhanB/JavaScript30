545492c57b50d67dd100fad4d674500aef8d6487
harshavardhan <harshavardhan.b7@outlook.com>
2020-02-02 12:52:24 +0530

    completed drumkit

--- 01 - JavaScript Drum Kit/drumkit.js
+++ 01 - JavaScript Drum Kit/drumkit.js
@@ -1,29 +1,36 @@
 function ready() {
-    var allElements = document.getElementsByClassName('key');
-    for(var i=0;i<allElements.length;i++)
+ 
+  document.addEventListener('keyup', function (e) {
+    var pressKeyCode = e.keyCode;
+    var container = document.querySelector(".keys");
+    var selectElement = container.querySelector(`div[data-key='${pressKeyCode}']`);
+    var selectAudioElement = document.querySelector(`audio[data-key='${pressKeyCode}']`);
+    if(selectElement != null)
     {
-        // allElements[i].addEventListener('click',function(e)
-        // {
-        //     console.log(this);
-        //     console.log(e.getAttribute('data-Key'));
-        // },false);
-        allElements[i].addEventListener('click',attachEvent());
+      selectElement.classList.remove('playing');
+      selectAudioElement.pause();
     }
-    function attachEvent()
+  });
+  document.addEventListener('keydown', function (e) {
+    var pressKeyCode = e.keyCode;
+    var container = document.querySelector(".keys");
+    var selectElement = container.querySelector(`div[data-key='${pressKeyCode}']`);
+    var selectAudioElement = document.querySelector(`audio[data-key='${pressKeyCode}']`);
+    if(selectElement != null)
     {
-        console.log('hi');
+      selectElement.classList.add('playing');
+      selectAudioElement.play();
     }
-    //document.getElementsByClassName('key')
-    //console.log(getid.getAttribute('id'));
-   }
+  });
+}
 
-  
-   
-   // this is required for the (not so) edge case where your script is loaded after the document has loaded
-   // https://developer.mozilla.org/en/docs/Web/API/Document/readyState
-   if (document.readyState !== 'loading') {
-     ready()
-   } else {
-     // the document hasn't finished loading/parsing yet so let's add an event handler
-     document.addEventListener('DOMContentLoaded', ready)
-   }
+
+
+// this is required for the (not so) edge case where your script is loaded after the document has loaded
+// https://developer.mozilla.org/en/docs/Web/API/Document/readyState
+if (document.readyState !== 'loading') {
+  ready()
+} else {
+  // the document hasn't finished loading/parsing yet so let's add an event handler
+  document.addEventListener('DOMContentLoaded', ready)
+}
